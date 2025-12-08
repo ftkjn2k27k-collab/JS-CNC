feab5102a75d3c9d7cc0f65c6e2d1c639c1bcdad
ftkjn2k27k-collab <ftkjn2k27k@privaterelay.appleid.com>
2025-12-08 18:36:27 +0000

    datatypes conversion

---
+++ 01_basics/03_datatypesconversion.js
@@ -0,0 +1,13 @@
+let score = "33"
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber ) 
+// if we use score as 33abc after valueInNumber it will display NaN as output for null "0 " and for undefined output will be NaN  same for string
+let isLoggedIn = 1
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn )
+// "" => false
+//"sarthak" => true 
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
