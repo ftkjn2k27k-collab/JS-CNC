c2656be82a652b3e515ee110d4ace90d664cc4ee
ftkjn2k27k-collab <ftkjn2k27k@privaterelay.appleid.com>
2025-12-08 18:13:34 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict" // Treat all js code as new version
+// datatypes type number,string,boolean,null,undefined,symbol(for uniqueness in react),object
+console.log(typeof "sarthak")
+console.log(typeof null)
+console.log(typeof undefined)
