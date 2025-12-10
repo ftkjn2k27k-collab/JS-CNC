cfdc28c1fdca48c696052a66bddf61edb0d648f0
ftkjn2k27k-collab <ftkjn2k27k@privaterelay.appleid.com>
2025-12-10 04:43:08 +0000

    comaparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,7 @@
+console.log("2">1)
+console.log(null>0)
+console.log(null>=0) // Comparisions convert null into a number treating it as zero == check is differnt from>= <= 
+console.log(null==0)
+console.log(undefined==0)
+console.log("2"===2)  // === strictly check value and datatype
+
