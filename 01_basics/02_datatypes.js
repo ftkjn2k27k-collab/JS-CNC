2ef8fb97229d316c43342093468dce1c714d365c
ftkjn2k27k-collab <ftkjn2k27k@privaterelay.appleid.com>
2025-12-10 05:11:31 +0000

    datatypes full

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,3 +3,18 @@
 console.log(typeof "sarthak")
 console.log(typeof null)
 console.log(typeof undefined)
+
+
+//  types ==>.  Primitive(change in copy not in original )
+// 7 types : String,number,boolean,null,undefined,symbol,BigInt
+
+// Reference type/ Non Primitive(Values allocated directly in memory)
+// Type of N.P. give object always (For functions it is object function)
+//  Objects,arrays,functions
+
+const id = Symbol("123")
+const anotherId = Symbol("123")   // Unique
+console.log(id==anotherId)
+
+// Arrays [" "], {name:"SARTHAK",age: "20"}
+
